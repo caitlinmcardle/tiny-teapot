0bb371ecaf5d5cab41a9158f63c7a91339112d99
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-01 21:44:05 +0100

    Added video to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Homepage() {
+  const video = require("../img/video.mp4");
   return (
     <main className="main">
-      <p>This is home</p>
+      <video id="background-video" loop autoPlay>
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </main>
   );
 }
