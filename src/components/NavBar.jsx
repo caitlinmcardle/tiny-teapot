964bf2291831705567a81e29260cd2563b460214
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-01 21:01:17 +0100

    styled nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "@reach/router";
 
 export default function NavBar() {
+  const logo = require("../img/logo-final.png");
   return (
     <nav className="nav">
       <ul className="nav-ul">
         <li className="nav-li">
           <Link className="nav-link" key="home" to="/">
-            Home
+            <img alt="logo" src={logo} width="80" height="80" />
           </Link>
         </li>
         <li className="nav-li">
