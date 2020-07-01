47a829eabcec416ff0bc4544fe64af658ab7da6a
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-01 15:46:28 +0100

    Set up NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <nav className="nav">
+      <ul className="nav-ul">
+        <li className="nav-li">
+          <Link className="nav-link" key="home" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link className="nav-link" key="about" to="/about">
+            About
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link className="nav-link" key="services" to="/services">
+            Services
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link className="nav-link" key="portfolio" to="/portfolio">
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-li">
+          <Link className="nav-link" key="contact" to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
