68e6871548f40a4e6b31d9f7081b2bf32af7e264
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-02 11:08:17 +0100

    Added content for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,45 @@ import React from "react";
 export default function About() {
   return (
     <main className="main">
-      <p>This is About</p>
+      <h1>About</h1>
+      <p>56 cups of tea consumed this week</p>
+      <p>179 lines of code written</p>
+      <p>
+        At Tiny Teapot we want to create beautful looking websites that are easy
+        to interact with and will help your business grow.
+      </p>
+      <p>
+        We understand how important a website is to your business, so we won't
+        bombard you with technical language and will work closely with you each
+        step of the way. You can provide as much or as little input as you would
+        like, and we can cater to businesses of any size and budget.
+      </p>
+      <p>
+        We love tea (if you hadn't already guessed) and would be happy to put
+        the kettle on and have a chat with you (either remotely or in person).
+        Drop us a message here.
+      </p>
+      <h2>Why your website is so important</h2>
+      <p>
+        It takes 0.05 seconds for users to form an opinion about your website
+      </p>
+      <p>
+        57% of users won't recommend a business with a poorly designed mobile
+        website
+      </p>
+      <p>
+        37% of people will stop enaging with a website if the content is
+        unattractive
+      </p>
+      <p>
+        75% of consumers judge a company's credibility based on it's website
+        design
+      </p>
+      <p>
+        {" "}
+        6 seconds: the average time a user spends looking at the content of a
+        website
+      </p>
     </main>
   );
 }
