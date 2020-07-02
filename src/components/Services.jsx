86bfa7c85f945f9bf43e93caa887e5291d79cfe3
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-02 11:30:59 +0100

    Added services content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,28 @@ import React from "react";
 export default function Services() {
   return (
     <main className="main">
-      <p>This is services</p>
+      <h1>Services</h1>
+      <h3>Web Design</h3>
+      <p>
+        We design beautiful, clean sites that provide a great user experience.
+        We can also provide a range of graphic design services.
+      </p>
+      <h3>Web Development</h3>
+      <p>
+        We use React (the same thing used to create Facebook) to produce fast,
+        reliable and scalable websites that are SEO friendly.
+      </p>
+      <h3>Maintenance</h3>
+      <p>
+        We won't just leave you in the dark, when we finish we can show you how
+        to make basic updates and changes to your site. We also offer
+        maintenance packages so that you don't have to worry.
+      </p>
+      <h3>SEO</h3>
+      <p>
+        We can optimize your site to increase traffic from search engines, so
+        that your clients are directed away from competitors and towards you.
+      </p>
     </main>
   );
 }
