d5ff69e7c47acf1f90fac90f9ad481c2314c78bc
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-07-02 10:12:53 +0100

    Styled homepage - added button to link to about section

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 export default function Homepage() {
   const video = require("../img/video.mp4");
   return (
     <main className="main">
-      <video id="background-video" loop autoPlay>
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="video-container">
+        <video className="background-video" loop autoPlay>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="video-content">
+          <Link className="homepage-link" key="about" to="/about">
+            <button className="homepage-button">Start your journey</button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
