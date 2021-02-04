b099a040d18717f29a9c195dbf606b76482c0500
caitlinmcardle <cait.e.mcardle@gmail.com>
2021-02-03 21:27:16 -0700

    basic portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 export default function Portfolio() {
   return (
-    <div>
+    
       <section id="portfolio-section">
-        <h1>Portfolio</h1>
+        <h1 id="portfolio-h1">Portfolio</h1>
+        <section id="portfolio-content">
+        <img className="portfolio-example" src={require('../img/portfolio-example.jpg')} alt="portfolio-example" />
+        <img className="portfolio-example" src={require('../img/portfolio-example.jpg')} alt="portfolio-example" />
+        <img className="portfolio-example" src={require('../img/portfolio-example.jpg')} alt="portfolio-example" />
+        </section>
+        
       </section>
-    </div>
+    
   );
 }
+
+
