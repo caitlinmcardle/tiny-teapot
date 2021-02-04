58f387f482f5b8218c076c3c09487dfdcacef2c8
caitlinmcardle <cait.e.mcardle@gmail.com>
2021-02-03 20:29:25 -0700

    How we work section styled

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -2,10 +2,36 @@ import React from "react";
 
 export default function How() {
   return (
-    <div>
+    
       <section id="how-section">
         <h1>How We Work</h1>
+        <section id="how-content">
+        <section className="how-steps">
+          <h4>Step 1</h4><p className="how-p">Tell us about you! Email us or fill out this <a href="#contact-section">form</a> so we can get an idea of what you are looking for</p>
+        </section>
+        <section className="how-steps">
+          <h4>Step 2</h4><p className="how-p">We'll arrange a free video consultation to get a better understanding of your needs</p>
+        </section>
+        <section className="how-steps">
+          <h4>Step 3</h4><p className="how-p">Following the call we will put together a proposal, this will include mock-ups of your website and a quote</p>
+        </section>
+        <section className="how-steps">
+          <h4>Step 4</h4><p className="how-p">Development begins. You provide us with the text content and we will start bringing it to life.</p>
+        </section>
+        <section className="how-steps">
+          <h4>Step 5</h4><p className="how-p">We will present the final draft and give you the opportunity to make any minor modifactions</p>
+        </section>
+        <section className="how-steps">
+          <h4>Step 6</h4><p className="how-p">We help you buy a domain name and set up hosting</p>
+        </section>
+        
+        
+        
+        
+        
+        
+        </section>        
       </section>
-    </div>
+    
   );
 }
