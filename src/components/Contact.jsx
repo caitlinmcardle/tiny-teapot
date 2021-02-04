0135af616aaeba0ad3d8cb41254555634c51b0dd
caitlinmcardle <cait.e.mcardle@gmail.com>
2021-02-03 19:43:06 -0700

    changed colors in contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Contact() {
   return (
     <section id="contact-section">
-      <h1>Get in touch</h1>
+      <h1 id="contact-h1">Get in touch</h1>
       <p className="email-p"><img id="email-img" src={require('../img/noun_email.png')} alt="email" /> caitlin@tinyteapotdesign.com</p>
-      <h2>Send us a message</h2>
+      <h2 id="contact-h2">Send us a message</h2>
       <form>
         <p className="table-row">
           <label for="name"></label>
